2a654456c7afae159aca9ee362f463196ff0276c
ilyas <deckersilyas@gmail.com>
2025-03-09 23:41:25 +0100

    Add TCP server as type

--- src/ServerType.php
+++ src/ServerType.php
@@ -7,4 +7,6 @@ class ServerType
     public const HTTP_SERVER = \Swoole\Http\Server::class;
 
     public const WS_SERVER = \Swoole\WebSocket\Server::class;
+
+    public const TCP_SERVER = \Swoole\Server::class;
 }
